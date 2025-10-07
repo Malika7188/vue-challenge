805d6b333311e4d78496c0911d9522a226801118
Malika7188 <asmanodeny@gmail.com>
2025-10-07 23:50:37 +0300

    Add basic form styling and layout

--- vue/src/create-user-form.vue
+++ vue/src/create-user-form.vue
@@ -4,3 +4,5 @@ import { ref, computed } from 'vue';
 // Reactive references to store username and password input values
 // These automatically update when the user types in the form fields
 const username = ref('');
+const password = ref('');
+
