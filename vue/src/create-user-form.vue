6b0a5839ab2f6ca7f1eb133346a8eccab882f902
Malika7188 <asmanodeny@gmail.com>
2025-10-07 23:49:55 +0300

    Add CreateUserForm component scaffold

--- vue/src/create-user-form.vue
+++ vue/src/create-user-form.vue
@@ -1,2 +1,7 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
+
+// Reactive references to store username and password input values
+// These automatically update when the user types in the form fields
+const username = ref('');
+const password = ref('');
